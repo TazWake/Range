781d6a57dcae9f23f3ef21ea97d5ea55a63c7312
Taz Wake <t.wake@halkynconsulting.co.uk>
2024-08-11 22:31:12 +0100

    updates

--- v2/entrypoint.sh
+++ v2/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+
 /etc/init.d/elasticsearch start
+
 sleep 30
+
 /opt/elasticsearch/bin/elasticsearch-users useradd investigator -p forensics -r superuser
+
 exec /usr/local/bin/start.sh
